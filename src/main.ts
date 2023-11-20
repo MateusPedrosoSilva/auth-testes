fad770ae3bd9b190df5ce268c2861b3cc1e8ae84
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2023-11-20 18:56:12 -0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const config = new DocumentBuilder()
+    // .addBearerAuth()
+    .setTitle('XMLs Grupo Líder')
+    .setDescription('API para projeto de XMLs do Grupo Líder')
+    .setVersion(process.env.APP_VERSION)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(process.env.APP_API_ROUTE, app, document);
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
